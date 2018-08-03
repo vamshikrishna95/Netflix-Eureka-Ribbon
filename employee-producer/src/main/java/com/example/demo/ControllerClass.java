2e1c5bf1e9de6001404987ebc50cd27dd9773307
vamshikrishna95 <vamshikrishna051995@gmail.com>
2018-08-03 11:26:43 +0530

    Hystrix FallBackMethod and CircuitBreaker

--- employee-producer/src/main/java/com/example/demo/ControllerClass.java
+++ employee-producer/src/main/java/com/example/demo/ControllerClass.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -9,14 +8,17 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 public class ControllerClass {
 
 	@RequestMapping("/test")
-	/*for hystrix 
+	/*for hystrix(features - fallback, circiut breaker)
 	1. Add hystrix dependency in producer pom file
 	2. Add @EnableCircuitBreaker in main class
 	3. Add @HystrixCommand(fallbackMethod = "test1") 
 	4. within double quotes enter the method to which it want to transfer.
+	5. when the method continously calls the fallbackmethod then the hystrix circuit will break at some time and 
+		consumer instead of calling test method it will call test1 method
 	*/
 	@HystrixCommand(fallbackMethod = "test1")
 	public EmployeeDTO test() {
+		System.err.println("Inside test");
 		EmployeeDTO dto = new EmployeeDTO("123", "Vamshi", "23", "123456789");
 		if (dto.getName().equalsIgnoreCase("Vamshi"))
 			throw new RuntimeException();
@@ -24,6 +26,7 @@ public class ControllerClass {
 	}
 
 	public EmployeeDTO test1() {
+		System.err.println("Inside Fallback");
 		EmployeeDTO dto = new EmployeeDTO("111", "Krishna", "25", "789456123");
 		return dto;
 	}
